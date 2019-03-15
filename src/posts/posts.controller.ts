3185044c340a2dfcf5bbccacd320c1330d6e6118
wanghao8080 <117663444@qq.com>
2019-03-15 12:48:32 +0800

    请求（Request）

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
   @Get()
-  index() {
+  index(@Req() request) {
+    console.log(
+      request.ip,
+      request.hostname,
+      request.method
+    )
+
     return [
       {
         title: 'hello ~'
