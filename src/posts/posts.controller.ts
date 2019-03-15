7dac67bcc099153fe636fb5a42830ea8fdceb43f
wanghao8080 <117663444@qq.com>
2019-03-15 13:44:31 +0800

    请求主体（@Body）

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Query, Headers, Param } from '@nestjs/common';
+import { Controller, Get, Req, Query, Headers, Param, Post, Body } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
@@ -18,5 +18,10 @@ export class PostsController {
     return {
       title: `Post ${params.id}`
     }
+  }
+
+  @Post()
+  store(@Body() body) {
+    console.log(body)
   }
 }
