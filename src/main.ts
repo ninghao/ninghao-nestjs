b9855044eb10fdfb098213d72142f8fd779c34e2
wanghao8080 <117663444@qq.com>
2019-03-19 10:31:41 +0800

    绑定异常过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DemoFilter } from './core/filters/demo.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new DemoFilter());
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Req, Query, Headers, Param, Post, Body, HttpException, HttpStatus, ForbiddenException } from '@nestjs/common';
+import { Controller, Get, Req, Query, Headers, Param, Post, Body, HttpException, HttpStatus, ForbiddenException, UseFilters } from '@nestjs/common';
 import { CreatePostDto } from './post.dto';
 import { DemoService } from './providers/demo/demo.service';
+import { DemoFilter } from 'src/core/filters/demo.filter';
 
 @Controller('posts')
+// @UseFilters(DemoFilter)
 export class PostsController {
   constructor(private readonly demoService: DemoService) { }
 
@@ -19,6 +21,7 @@ export class PostsController {
   }
 
   @Post()
+  // @UseFilters(DemoFilter)
   store(@Body() post: CreatePostDto) {
     // throw new HttpException('没有权限！', HttpStatus.FORBIDDEN);
     throw new ForbiddenException('没有权限！');
