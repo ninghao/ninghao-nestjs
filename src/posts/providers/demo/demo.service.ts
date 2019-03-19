73aef1b5a2d604d4b04456eb838fc11ca5ca11f6
wanghao8080 <117663444@qq.com>
2019-03-19 08:18:08 +0800

    定义服务方法

--- src/posts/providers/demo/demo.service.ts
+++ src/posts/providers/demo/demo.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from 'src/posts/interfaces/post.interface';
 
 @Injectable()
-export class DemoService {}
+export class DemoService {
+  private readonly posts: Post[] = [];
+
+  findAll(): Post[] {
+    return this.posts;
+  }
+
+  create(post: Post) {
+    this.posts.push(post);
+  }
+}
