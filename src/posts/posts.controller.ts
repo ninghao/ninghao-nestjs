9e62d820a670a0d36ef4df1603526b439230336a
wanghao8080 <117663444@qq.com>
2019-03-19 08:25:22 +0800

    使用服务

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Get, Req, Query, Headers, Param, Post, Body } from '@nestjs/common';
 import { CreatePostDto } from './post.dto';
+import { DemoService } from './providers/demo/demo.service';
 
 @Controller('posts')
 export class PostsController {
-  @Get()
-  index(@Headers('authorization') headers) {
-    console.log(headers)
+  private readonly demoService;
 
-    return [
-      {
-        title: 'hello ~'
-      }
-    ];
+  constructor(demoService: DemoService) {
+    this.demoService = demoService;
+  }
+
+  @Get()
+  index() {
+    return this.demoService.findAll();
   }
 
   @Get(':id')
@@ -23,6 +24,6 @@ export class PostsController {
 
   @Post()
   store(@Body() post: CreatePostDto) {
-    console.log(post.title)
+    this.demoService.create(post);
   }
 }
