8be4769aeddd3019df61460d7639d59e295d5a97
wanghao8080 <117663444@qq.com>
2019-03-19 10:02:45 +0800

    使用中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './modules/posts/posts.module';
+import { DemoMiddleware } from './core/middlewares/demo.middleware';
 
 @Module({
   imports: [PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(DemoMiddleware)
+      .forRoutes('posts');
+  }
+}
