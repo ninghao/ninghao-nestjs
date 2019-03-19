d2f763303aa58631494d324ff8d27e43d1ce03d8
wanghao8080 <117663444@qq.com>
2019-03-19 08:09:58 +0800

    服务（Services）

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
+import { DemoService } from './posts/providers/demo/demo.service';
 
 @Module({
   imports: [],
   controllers: [AppController, PostsController],
-  providers: [AppService],
+  providers: [AppService, DemoService],
 })
 export class AppModule {}
