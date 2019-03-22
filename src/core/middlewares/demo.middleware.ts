4894ad4ca2f5ddecf3b56611b487567a36e424ea
wanghao8080 <117663444@qq.com>
2019-03-22 13:31:45 +0800

    使用中间件在请求中添加数据

--- src/core/middlewares/demo.middleware.ts
+++ src/core/middlewares/demo.middleware.ts
@@ -3,7 +3,20 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class DemoMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    console.log('hello ~');
+    req.user = {
+      roles: [
+        'guest'
+      ]
+    }
+
+    if (req.header('x-demo') === 'secret') {
+      req.user = {
+        roles: [
+          'member'
+        ]
+      }
+    }
+
     next();
   }
 }
