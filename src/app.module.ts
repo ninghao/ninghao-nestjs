8a67cd8f6d98c2add940f7e17a13ca234bccdcab
wanghao8080 <117663444@qq.com>
2019-04-07 10:08:20 +0800

    数据库连接配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest',
+      password: 'password',
+      database: 'nest',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity.ts']
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
