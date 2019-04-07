6e7117a2d5fb03b5778edb21951a99a3348a6fc5
wanghao8080 <117663444@qq.com>
2019-04-07 13:12:03 +0800

    把 TypeORM 配置放在环境变量里（.env）

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,7 @@ import { PostModule } from './modules/post/post.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'nest',
-      password: 'password',
-      database: 'nest',
-      synchronize: true,
-      entities: [__dirname + '/**/*.entity.ts']
-    }),
+    TypeOrmModule.forRoot(),
     PostModule
   ],
   controllers: [AppController],
