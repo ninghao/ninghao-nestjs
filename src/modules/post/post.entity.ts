89ca92c695c163018ebeaea134c17d4d47803e38
wanghao8080 <117663444@qq.com>
2019-04-07 11:10:08 +0800

    实体里的数据栏（Column）

--- src/modules/post/post.entity.ts
+++ src/modules/post/post.entity.ts
@@ -1,7 +1,19 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class Post {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  title: string;
+
+  @Column('longtext')
+  body: string;
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
 }
