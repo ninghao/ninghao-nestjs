4acd6bbecd5fd71bd7dd2941e58788d10a24a28b
wanghao8080 <117663444@qq.com>
2019-04-08 21:13:25 +0800

    创建模块（User）

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './modules/post/post.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
-    PostModule
+    PostModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
