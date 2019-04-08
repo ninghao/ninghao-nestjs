9c061afd219ec1b06bf7b64256675d72a787bd6b
wanghao8080 <117663444@qq.com>
2019-04-08 11:20:21 +0800

    注册实体的 Repository

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './post.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Post])
+  ],
   controllers: [PostController],
   providers: [PostService]
 })
-export class PostModule {}
+export class PostModule { }
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>
+  ) { }
+}
