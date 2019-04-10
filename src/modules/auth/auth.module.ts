5e3039da4976e672c09483a84c99b6d8d1d26a6e
wanghao8080 <117663444@qq.com>
2019-04-10 14:37:41 +0800

    Passport.js

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
@@ -12,6 +13,9 @@ import { UserModule } from '../user/user.module';
       signOptions: {
         expiresIn: '12h'
       }
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt'
     })
   ],
   controllers: [AuthController],
