cb220c9443d0cf858a6fad1c152d66537f79c857
wanghao8080 <117663444@qq.com>
2019-04-10 08:16:59 +0800

    在 Auth 模块中使用 User 模块里的服务

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService
+  ) { }
+}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -9,6 +9,7 @@ import { User } from './user.entity';
     TypeOrmModule.forFeature([User])
   ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService]
 })
 export class UserModule { }
