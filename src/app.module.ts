92c469fc7dcf428d873a58920ef05fd6fc59cb56
wanghao8080 <117663444@qq.com>
2019-04-10 08:14:38 +0800

    创建身份验证模块（Auth）

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './modules/post/post.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     PostModule,
-    UserModule
+    UserModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
