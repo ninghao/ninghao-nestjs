ca87a904a895aaff605f2dc4c6879cf1b3c8c426
wanghao8080 <117663444@qq.com>
2019-04-10 14:34:46 +0800

    签发 JWT（jwtService.sign）

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { LoginDto } from './auth.dto';
+import { JwtPayload } from './auth.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly userService: UserService
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService
   ) { }
 
   async login(data: LoginDto) {
@@ -20,6 +23,17 @@ export class AuthService {
       throw new UnauthorizedException('密码不匹配。');
     }
 
-    return entity;
+    const { id } = entity;
+    const payload = { id, name };
+    const token = this.signToken(payload);
+
+    return {
+      ...payload,
+      token
+    }
+  }
+
+  signToken(data: JwtPayload) {
+    return this.jwtService.sign(data);
   }
 }
