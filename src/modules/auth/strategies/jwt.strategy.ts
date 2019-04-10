9b6bdd109b4ff4c4b93a02d23f394ff5b696aafe
wanghao8080 <117663444@qq.com>
2019-04-10 15:01:00 +0800

    验证身份的方法（validate）

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,19 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy, ExtractJwt, VerifiedCallback } from "passport-jwt";
 import { JwtPayload } from "../auth.interface";
+import { UserService } from "src/modules/user/user.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: 'qHqPHVPasjfHDCrcX7Ao7x5O5W098RU3i6lloVgWZFY='
     })
   }
 
-  async validate(payload: JwtPayload, done: VerifiedCallback) {
+  async validate(payload: JwtPayload) {
     console.log('payload:', payload);
+    const { name } = payload;
+    const entity = await this.userService.findByName(name);
 
+    if (!entity) {
+      throw new UnauthorizedException('没找到用户。');
+    }
+
+    return entity;
   }
 }
