1618c26600d2b0fccaed39e0a69718f7ca4d26d1
wanghao8080 <117663444@qq.com>
2019-04-15 15:35:17 +0800

    分类实体

---
+++ src/modules/category/category.entity.ts
@@ -0,0 +1,15 @@
+import {
+  Entity, PrimaryGeneratedColumn, Column
+} from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  alias: string;
+}
