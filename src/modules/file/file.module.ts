0f5e79d8d3842b58ff20633982a0e662f8d8e333
wanghao8080 <117663444@qq.com>
2019-05-05 13:57:25 +0800

    文件上传配置选项（存储位置）

--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      dest: './uploads'
+    })
+  ],
   controllers: [FileController],
   providers: [FileService]
 })
-export class FileModule {}
+export class FileModule { }
